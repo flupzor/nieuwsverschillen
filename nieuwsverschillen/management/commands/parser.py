2e5cafd3e5af387ef4f46d695c4875a139637cc7
Alexander Schrijver <alex@flupzor.nl>
2013-08-31 22:03:08 +0200

    Add an util which re-parses the articles.

--- nieuwsverschillen/management/commands/parser.py
+++ nieuwsverschillen/management/commands/parser.py
@@ -1,7 +1,15 @@
 from django.core.management.base import BaseCommand
 
+from nieuwsverschillen.models import ArticleVariant
+from nieuwsverschillen.scraper import ArticleScraper
+
 class Command(BaseCommand):
     help = "Parser"
     def handle(self, *args, **options):
-        self.stdout.write("Parser")
+        # re-parse all the articles.
+
+        for variant in ArticleVariant.objects.all():
+            scraper = ArticleScraper(variant.article)
+
+            scraper.article_parser(variant)
 
