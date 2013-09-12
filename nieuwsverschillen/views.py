5318c76caadb6f3aadcd736d2a5ff4199367192c
Alexander Schrijver <alex@flupzor.nl>
2013-09-12 15:55:57 +0200

    Make a list of similar items for a NewsVariant.

--- nieuwsverschillen/views.py
+++ nieuwsverschillen/views.py
@@ -115,6 +115,8 @@ class DiffView(TemplateView):
         r1_obj = get_object_or_404(ArticleVariant, pk=r1)
         r2_obj = get_object_or_404(ArticleVariant, pk=r2)
 
+        context['r1'] = r1_obj
+        context['r2'] = r2_obj
         context['diff'] = self.diff_prettyHtml(r1_obj.diff(r2_obj))
 
         return context
