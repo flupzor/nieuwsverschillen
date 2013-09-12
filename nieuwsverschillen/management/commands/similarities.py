c580933c8a63c924fc08e936572af9ae87579791
Alexander Schrijver <alex@flupzor.nl>
2013-09-12 15:51:02 +0200

    Add some rudimentary logging.

--- nieuwsverschillen/management/commands/similarities.py
+++ nieuwsverschillen/management/commands/similarities.py
@@ -15,22 +15,41 @@
 from django.core.management.base import BaseCommand
 
 from django.conf import settings
+from django.utils import timezone
 
 from nieuwsverschillen.models import ArticleVariant
 
 import itertools
 
+import logging
+logger = logging.getLogger(__name__)
+
 class Command(BaseCommand):
     help = "similarities"
     def handle(self, *args, **options):
         variants = ArticleVariant.objects.all()
+        variants_count = variants.count()
+
+        logger.debug("Number of variants: {0}".format(variants_count))
 
         for v0, v1 in itertools.combinations(variants, 2):
+            # Skip if comparing the same article.
+            if v0.article == v1.article:
+                continue
+
             # Skip articles on the same site
             if v0.article.source == v1.article.source:
                 continue
 
-            if v0.is_similar(v1):
+            start_time = timezone.now()
+            is_similar = v0.is_similar(v1)
+            finish_time = timezone.now()
+
+            time_delta = finish_time - start_time
+
+            logger.debug("time_delta seconds: {0}".format(time_delta.total_seconds()))
+
+            if is_similar:
                 logger.debug("Found a similar version %d, %d", v0.pk, v1.pk)
 
                 v0.similar_versions.add(v1)
